a0ae1bfd3e2327fd470ed142e2feeacdad5dfb36
everton.mello <everton.mello@airstrip.com.br>
2018-07-30 23:27:45 -0300

    aa

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 //Install express server
 const express = require('express');
 const app = express();
-const path = require('path')
+
+app.use(express.static(__dirname + '/dist'));
 
 // Serve only the static files form the dist directory
-app.get('/', function(req,res){
-    var pat =  path.join(__dirname + '/dist/index.html')
-    res.sendFile(pat)
-});
 
 app.listen( '8080', function(){
     console.log('server on 8080')
